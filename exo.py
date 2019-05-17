6122782933d8c1b19249140c2fb915c97d34fe94
theoflaus <48955402+theoflaus@users.noreply.github.com>
2019-05-17 12:08:59 +0200

    Update exo.py

--- exo.py
+++ exo.py
@@ -1,5 +1,4 @@
 import math
-import requests
 
 def exercice_1(a, b):
     """Fonction qui renvoie le produit de 2 nombres
@@ -57,46 +56,3 @@ class Exercice_3:
             return True
         else:
             return False
-
-class Exercice_4:
-
-    def __init__(self, title):
-
-        self.URL = "https://fr.wikipedia.org/w/api.php"
-        self.S = requests.Session()
-        self.title = title
-
-    def get_title(self):
-
-        PARAMS = {
-            'action': "query",
-            'list': "search",
-            'srsearch': self.title,
-            'format': "json",
-            'srlimit': 1
-        }
-
-        R = self.S.get(url=self.URL, params=PARAMS)
-        DATA = R.json()
-
-        return DATA["query"]["search"][0]["title"]
-
-    def extract_info(self):
-
-        title = self.get_title()
-
-        PARAMS = {
-            'action': 'query',
-            'format': 'json',
-            'prop': 'extracts',
-            'titles': title,
-            'explaintext': True,
-            "exlimit": 1,
-            'exchars': 175
-        }
-
-        R = self.S.get(url=self.URL, params=PARAMS)
-        DATA2 = R.json()
-
-        for elt in DATA2["query"]["pages"].values():
-            return elt["extract"]
